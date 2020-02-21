349200ac0576dd0e71e11035c65029102f430b58
TSevillaCS <tsevilla12@gmail.com>
2020-02-21 10:13:32 -0800

    Create Program.cpp

---
+++ Program.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include "GradeBook.h"
+using namespace std;
+
+int main()
+{
+	GradeBook gradeBook1("CS101 intro to C++");
+	GradeBook gradeBook2("CS102 Data Structure in C++");
+
+	cout << "gradebook 1 created for course: " << gradeBook1.getCourseName() << endl;
+	cout << "gradebook 2 created for course: " << gradeBook2.getCourseName() << endl;
+}
