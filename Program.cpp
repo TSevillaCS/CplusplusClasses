4fda8da4a0a2cd24f987702b8cf4dd6a3503dd07
TSevillaCS <tsevilla12@gmail.com>
2020-02-21 10:14:55 -0800

    Update Program.cpp

--- Program.cpp
+++ Program.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 	GradeBook gradeBook1("CS101 intro to C++");
-	GradeBook gradeBook2("CS102 Data Structure in C++");
+	GradeBook gradeBook2("CS102 Data Structures in C++");
 
 	cout << "gradebook 1 created for course: " << gradeBook1.getCourseName() << endl;
 	cout << "gradebook 2 created for course: " << gradeBook2.getCourseName() << endl;
